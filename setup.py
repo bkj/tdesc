b5b2e05e7a2c8b82c034f259d0bfbfd2108fe4a6
bkj <ben@gophronesis.com>
2017-05-15 12:38:48 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python
+
+"""
+    Setup script for tdesc
+"""
+
+from setuptools import setup
+
+setup(
+    name='tdesc',
+    author='Ben Johnson',
+    author_email='bkj.322@gmail.com',
+    classifiers=[],
+    description='Image Descriptors',
+    keywords=['tdesc'],
+    license='ALV2',
+    version="0.0.1",
+    install_requires=[]
+)
