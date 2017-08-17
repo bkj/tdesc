bc025a6dbadb3176760226c7c11a3abfa697156c
bjohnson <ben@gophronesis.com>
2017-08-17 10:15:23 -0400

    make dlib import optional

--- tdesc/workers/__init__.py
+++ tdesc/workers/__init__.py
@@ -1,5 +1,12 @@
+import sys
+
 from base import BaseWorker
-from dlib_worker import DlibFaceWorker
-from dlib_batch_worker import DlibFaceBatchWorker
+
+try:
+    from dlib_worker import DlibFaceWorker
+    from dlib_batch_worker import DlibFaceBatchWorker
+except:
+    print >> sys.stderr, 'cannot load dlib workers'
+
 from vgg16_worker import VGG16Worker
 from yolo_worker import YoloWorker
